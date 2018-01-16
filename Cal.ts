cc48656264c0715f673a471b8fa46e61c43e437a
Joss <joss.flandrin@gmail>
2018-01-16 16:18:51 +0100

    Jim

--- Cal.ts
+++ Cal.ts
@@ -1,2 +1,12 @@
-npm install;
-alert("Hey!");
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person: Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = { firstName: "Jane", lastName: "User" };
+
+document.body.innerHTML = greeter(user);
